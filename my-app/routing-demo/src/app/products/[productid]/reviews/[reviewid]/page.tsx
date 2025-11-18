60a594404ab5f7fc22f3add1b8b361885092f985
divyaramesh4 <divyaramesh3105@gmail.com>
2025-11-18 20:04:23 +0530

    Stopped tracking the local demo template folder. It remains on remote.

--- my-app/routing-demo/src/app/products/[productid]/reviews/[reviewid]/page.tsx
+++
@@ -1,6 +0,0 @@
-export default async function ProductDetails({ params }: { 
-    params: { productid: string, reviewid: string};
-}) {
-const { productid, reviewid } = params;
-    return <div>review for {reviewid} Product ID is {productid}</div>;
-}
